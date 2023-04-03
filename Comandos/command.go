ae1ddc78bd0b3ddb47cffcf1651cd65ec4769bd7
erazoex <brian.erazosagastume@gmail.com>
2023-04-02 18:35:00 -0600

    algunos cambios

--- Comandos/command.go
+++ Comandos/command.go
@@ -1,11 +1,32 @@
 package comandos
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/erazoex/-MIA-Proyecto2/Datos/data"
+)
 
 type executer interface {
 	exe(parametros []string)
 }
 
-func writeMBR(master *MBR) {
-	fmt.Println(("hola mundo!"))
+func WriteMBR(master *data.MBR, path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	_, err = file.Write(master)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer file.Close()
 }
